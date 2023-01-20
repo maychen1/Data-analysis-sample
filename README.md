2de139c8d502511127126c2a08965c3e58a9073e
maychen1 <zchen10@uchicago.edu>
2023-01-19 18:20:22 -0600

    Most recent update
    Added README file and removed excess comments

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# code-samples
-Repository that stores demo works 
+# code-sample: Employment rate by industry 
+
+SAEMP25N are GDP and personal income data in the United States. 
+For this project, I want to reorder and analyze 2000 and 2017 SAEMP25N data in total and by industry. 
+
+__Clean data__ 
+This section cleans the data by removing the footnotes, drop unnecessary summaries, and remove N/As. 
+step1: remove footnotes for both data sets
+step2.1: Drop subtotal summaries in data2
+step2.2: Drop blanks/tabs in data2 `Description` column
+
+__Recognize data__
+This is the second preparation step, which aims to merge the two clean data sets together for further analysis. 
+step1: Apply pivot longer on both data frames to make year and total/subtotal two columns
+step2: combine reorganized data into one long dataframe 
+
+__Calculate statistics__
+Turn Subtotal into Percentage. 
+
+__Export the cleaned data__ 
+Store data in csv format. 
+
+__Data visualization__
+In this project, I select the 2000 top 5 employment rate in manufacturing industry and plot their changes. 
--- code sample_data cleaning and visualization.R
+++ code sample_data cleaning and visualization.R
@@ -1,5 +1,5 @@
 ###
-title: "Code Sample: Data and Programming; Data cleaning and analysis"
+title: "Data cleaning and analysis"
 author: "May(Zhaoyuan) Chen"
 ###
 rm(list = ls())
@@ -8,13 +8,10 @@ library(dplyr)
 library(stringr)
 
 ## Work Directory 
-general = "/Users/zhaoyuan/Documents/22F/PPHA Data and Programming II - R/Assisgnments/homework-1-maychen1"
+general = "/Users/zhaoyuan/Documents/Application/Applying/Code sample/code-samples"
 setwd(general)
 
 ## Read data
-# SAEMP25N are GDP and personal income data in the United States. 
-# For this project, I want to reorder and analyze 2000 and 2017 SAEMP25N data 
-# in total and by industry. 
 data_names <- c("SAEMP25N total.csv", "SAEMP25N by industry.csv")
 
 for (i in 1:length(data_names)) {
@@ -26,8 +23,6 @@ for (i in 1:length(data_names)) {
 }
 
 ## Clean data: 
-# This section cleans the data by removing the footnotes, drop unnecessary 
-# summaries, and remove N/As. 
 # step1: remove footnotes for both data sets
 data1 <- data1[-c(52:54),]
 data2 <- data2[-c(562:566),]
@@ -39,8 +34,6 @@ data2 <- filter(data2, Description != "By industry")
 data2$Description <- trimws(data2$Description , which = c("left"))
 
 ## Reorganize data: 
-# This is the second preparation step, which aims to merge the two clean data 
-# sets together for further analysis. 
 # step1: Apply pivot longer on both data frames to make year and total/subtotal 
 # two columns
 data1 <- data1 %>%
@@ -64,20 +57,18 @@ data2 <- data2 %>%
 data1$year <- as.numeric(str_extract(data1$year, "[0-9]+"))
 data2$year <- as.numeric(str_extract(data2$year, "[0-9]+"))
 
-# step 2: combine reorganized data into one long dataframe 
+# step2: combine reorganized data into one long dataframe 
 data <- left_join(data2, data1, by = c("GeoFips","GeoName", "year"))
 
 ## Calculate statistics: 
-# Turn Subtotal into Percentage
 data$subtotal <- as.numeric(data$subtotal)
 data          <- data %>% mutate(pct = subtotal/total)
 
 ## Export the cleaned data:
-output <- "/Users/zhaoyuan/Documents/22F/PPHA Data and Programming II - R/Assisgnments/homework-1-maychen1/data.csv"
+output <- "/Users/zhaoyuan/Documents/Application/Applying/Code sample/code-samples/data.csv"
 write.csv(data, output, row.names = F)
 
 ## Data visualization 
-# Select top 5 employment in manufacturing industry in 2000:
 h5_em_manufacture <- data %>%
   filter(year == "2000") %>%
   filter(Description == "Manufacturing") %>%
@@ -90,7 +81,6 @@ h5_em_manufacture_dt <- data %>%
   filter(Description == "Manufacturing")
 
 # Plot changes in employment in states that ranked top 5 in 2000
-library(ggplot2)
 ggplot(h5_em_manufacture_dt, aes(x = as.character(year), 
                                  y = pct,
                                  color = GeoName)) +  
